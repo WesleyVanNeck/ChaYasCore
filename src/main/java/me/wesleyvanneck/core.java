11dfd4d696e821280fdfaff16bce8960b6cfa19a
Rem <wesleyvanneck105@outlook.com>
2019-03-29 19:19:47 +0100

    Update core.java

--- src/main/java/me/wesleyvanneck/core.java
+++ src/main/java/me/wesleyvanneck/core.java
@@ -6,21 +6,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class core extends JavaPlugin {
 	
-     public static final String PREFIX = "[" + ChatColor.DARK_PURPLE + "ChaYasCore" + ChatColor.RESET + "] ";
-			
-	@Override
-	public void onEnable() {
-        ProtocolSupportSwordBlocking ProtocolSupportSwordBlocking = new ProtocolSupportSwordBlocking();  
-		Bukkit.getPluginManager().registerEvents(this, this);
-		Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_PURPLE + "Starting ChaYasCore!" + ChatColor.RESET);
-	}
-	
-	@Override
-	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_PURPLE + "disableing ChaYasCore!" + ChatColor.RESET);
-   }
+	public void (Player player, OfflinePlayer targetPlayer, int monthId) {
+		long delay = 0L;
+		if (CraftGo.Player.isPocketPlayer(player)) {
+			player.closeInventory();
+			delay = LinkServer.getInventoryUpdateDelay();
+		}
 }
